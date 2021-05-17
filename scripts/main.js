994fad4a4bc7c91d7e562b607db8fdfc18614bda
jesseygalje <galjejessey@hotmail.nl>
2021-05-17 17:18:15 +0200

    laatste

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = -6;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -9,18 +9,18 @@ function spin(){
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, -16); //equivalent to 60 fps
+setInterval(spin, 16); //equivalent to 60 fps
 
 function spiny(){
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.y);
 }
 
-setInterval(spiny, -16); //equivalent to 60 fps
+setInterval(spiny, 16); //equivalent to 60 fps
 
 function spinz(){
 	myOtherBox.object3D.rotation.z += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.z);
 }
 
-setInterval(spinz, -16); //equivalent to 60 fps
+setInterval(spinz, 16); //equivalent to 60 fps
